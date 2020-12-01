086c5743a9fc19193cee1a02f0b0c4dea8b897cd
Matt Peperell <matt@peperell.com>
2020-12-01 14:03:34 +0000

    [2015:2] solved part 2

--- 2015/2.pl
+++ 2015/2.pl
@@ -1,6 +1,15 @@
 use strict;
 use List::Util qw[max sum];
 
+sub ribbon_required {
+	my ($x, $y, $z) = @_;
+	my @lengths = sort { $a <=> $b } ($x, $y, $z);
+
+	my $needed = $x*$y*$z; # bow
+	$needed += 2 * ($lengths[0] + $lengths[1]); # wrapping
+	return $needed;
+}
+
 sub paper_required {
 	my ($x, $y, $z) = @_;
 	my @lengths = sort { $a <=> $b } ($x, $y, $z);
@@ -12,10 +21,13 @@ sub paper_required {
 }
 
 my $paper = 0;
+my $ribbon = 0;
 while (<>) {
 	chomp;
         my ($a, $b, $c) = split /x/;
 	$paper += paper_required($a, $b, $c);
+	$ribbon += ribbon_required($a, $b, $c);
 }
 
 print "total paper needed at end is $paper\n";
+print "total ribbon needed at end is $ribbon\n";
