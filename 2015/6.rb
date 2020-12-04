5681834df745de041966324f6c1b8cdf2dd327d1
Matt Peperell <matt@peperell.com>
2020-12-04 17:54:51 +0000

    [2015:6] solved part 2

--- 2015/6.rb
+++ 2015/6.rb
@@ -2,6 +2,7 @@ input = ARGF.readlines.map(&:strip)
 SIZE = 1000
 PATTERN = /^(?<operation>(turn (on|off))|(toggle)) (?<left>\d+),(?<top>\d+) through (?<right>\d+),(?<bottom>\d+)$/
 
+# part 1
 grid = Array.new(SIZE) { Array.new(SIZE, false) }
 
 input.each_with_object(grid) do |record, grid|
@@ -21,3 +22,26 @@ input.each_with_object(grid) do |record, grid|
 end
 
 puts "Part 1: #{grid.flatten.filter{|x| x}.count}"
+
+
+# part 2
+grid = Array.new(SIZE) { Array.new(SIZE, 0) }
+
+input.each_with_object(grid) do |record, grid|
+  record.match(PATTERN) do |m|
+    left, top, right, bottom = %i[left top right bottom].map { |x| m[x].to_i }
+    # puts "DBG: #{m[:operation]} between #{[left, top]} and #{[right, bottom]}"
+    (top..bottom).each do |row|
+      (left..right).each do |col|
+        case m[:operation]
+        when 'turn on' then grid[row][col] += 1
+        when 'turn off' then grid[row][col] -= 1
+        when 'toggle' then grid[row][col] += 2
+        end
+        grid[row][col] = 0 if grid[row][col].negative?
+      end
+    end
+  end
+end
+
+puts "Part 2: #{grid.flatten.sum}"
