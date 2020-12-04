8c56843b3fc218c1924294083a5270c5b3edd49e
Matt Peperell <matt@peperell.com>
2020-12-04 11:24:55 +0000

    [2020:4] Solved part 2

--- 2020/4.rb
+++ 2020/4.rb
@@ -21,11 +21,32 @@ def text_to_hash text
   end
 end
 
-def valid? record
+def valid? record, strict=false
   required =%w[ byr iyr eyr hgt hcl ecl pid ]
   # optional = %i[ cid ]
-  required.all? { |field| record.has_key? field }
+  return false unless required.all? { |field| record.has_key? field }
+  return true unless strict
+  return false unless (1920..2002).include? record['byr'].to_i
+  return false unless (2010..2020).include? record['iyr'].to_i
+  return false unless (2020..2030).include? record['eyr'].to_i
+
+  unit = record['hgt'][-2,2]
+  value = record['hgt'][0...-2]
+  case unit
+  when 'cm'
+    return false unless (150..193).include? value.to_i
+  when 'in'
+    return false unless (59..76).include? value.to_i
+  else
+    return false #unknown unit
+  end
+
+  return false unless record['hcl'] =~ /^#[0-9a-f]{6}$/
+  return false unless %w[amb blu brn gry grn hzl oth].include? record['ecl']
+  return false unless record['pid'] =~ /^\d{9}$/
+  true
 end
 
 records = split_records data
 puts "Part 1: #{records.filter { |r| valid? r }.count}"
+puts "Part 2: #{records.filter { |r| valid?(r, 'strict') }.count}"
