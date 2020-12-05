776a8855aa183b20a188470501bf05f0e4b58607
Matt Peperell <matt@peperell.com>
2020-12-05 15:43:18 +0000

    [2019:1] solved part 2

--- 2019/1.rb
+++ 2019/1.rb
@@ -1,5 +1,21 @@
 masses = ARGF.readlines.map &:to_i
 
-fuel = masses.sum { |mass| (mass / 3).floor - 2 }
+def fuel_for_mass mass
+  (mass / 3).floor - 2 
+end
 
-puts "Part 1: #{fuel}"
+fuel1 = masses.sum { |mass| fuel_for_mass mass }
+
+puts "Part 1: #{fuel1}"
+
+fuel2 = masses.sum do |mass|
+  fuel = 0
+
+  while ( extra = fuel_for_mass(mass) ) > 0
+    fuel += extra
+    mass = extra
+  end
+  fuel
+end
+
+puts "Part 2: #{fuel2}"
