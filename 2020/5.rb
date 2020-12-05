d1384fa801d0806567909a93e3571f6322ed7e2c
Matt Peperell <matt@peperell.com>
2020-12-05 11:34:01 +0000

    [2020:5] solved part 2

--- 2020/5.rb
+++ 2020/5.rb
@@ -10,3 +10,7 @@ end
 ids = codes.map { |code| seatcode_to_id code }
 
 puts "Part 1: #{ids.max}"
+
+my_id = ids.sort.each_cons(2).reject { |a,b| b == a+1 }.first.first + 1
+
+puts "Part 2: #{my_id}"
