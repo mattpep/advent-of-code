dfeb24431f5b3dddef574b6d311b9668930932ac
Matt Peperell <matt@peperell.com>
2020-12-10 01:23:36 +0000

    [2020:1] refactor

--- 2020/1.rb
+++ 2020/1.rb
@@ -2,39 +2,11 @@ require 'net/http'
 
 TARGET = 2020
 
-NUMBERS = ARGF.read.split("\n").map(&:to_i)
+numbers = ARGF.read.split("\n").map(&:to_i)
 
-# Though in my input there are no duplicated numbers, we don't know whether
-# that's the general case. So we dedupe by index rather than value
-def part1
-  NUMBERS.each_with_index do |x, idx_x|
-    next if x > TARGET
-    NUMBERS.each_with_index do |y, idx_y|
-      next if idx_x == idx_y
-      if x + y == TARGET
-        puts "Part 1 solution: Found a match for #{x} and #{y}. Product is #{x*y}"
-        return true
-      end
-    end
-  end
-end
+part1 = numbers.combination(2).select { |a,b| a+b == TARGET }.first.reduce :*
 
-def part2
-  NUMBERS.each_with_index do |x, idx_x|
-    next if x > TARGET
-    NUMBERS.each_with_index do |y, idx_y|
-      next if idx_x == idx_y
-      next if x + y > TARGET
-      NUMBERS.each_with_index do |z, idx_z|
-        next if idx_x == idx_z || idx_y == idx_z
-        if x + y + z == TARGET
-          puts "Part 2 solution: Found a match for #{x}, #{y} and #{z}. Product is #{x*y*z}"
-          return true
-        end
-      end
-    end
-  end
-end
+part2 = numbers.combination(3).select { |a,b,c| a+b+c == TARGET }.first.reduce :*
 
-part1 || puts("No solution found for part 1 :-(") && exit(1)
-part2 || puts("No solution found for part 2 :-(") && exit(1)
+puts "Part 1: #{part1}"
+puts "Part 2: #{part2}"
