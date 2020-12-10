2bbec5e61ed76b0f592be1ec4738f0935b66a4c6
Matt Peperell <matt@peperell.com>
2020-12-10 01:17:05 +0000

    [2020:8] Solved part 2

--- 2020/8.rb
+++ 2020/8.rb
@@ -4,21 +4,40 @@ bootcode = ARGF.readlines.map do |line|
   [op, data]
 end
 
-acc = 0
-pc = 0
-seen = []
+INFINITE = 1
+TERMINATE = 0
 
-while true
-  break if seen.include? pc
-  seen << pc
-  case bootcode[pc][0]
-  when 'nop' then  pc += 1
-  when 'acc' then  acc += bootcode[pc][1]; pc += 1
-  when 'jmp' then  pc += bootcode[pc][1]
-  else
-    puts "Invalid opcode #{bootcode[pc][0]} in instruction: #{bootcode[pc]} at address #{pc}"
+def execute program
+  acc = 0
+  pc = 0
+  seen = []
+  while pc < program.size
+    break if seen.include? pc
+    seen << pc
+    case program[pc][0]
+    when 'nop' then  pc += 1
+    when 'acc' then  acc += program[pc][1]; pc += 1
+    when 'jmp' then  pc += program[pc][1]
+    end
+    break if pc >= program.size
   end
+  completion = pc >= program.size ? TERMINATE : INFINITE
+  [completion, acc]
 end
 
-puts "Part 1: #{ acc }"
+puts "Part 1: #{ execute(bootcode).last }"
 
+bootcode.each_with_index do |instr, addr|
+  corruption = Marshal.load Marshal.dump bootcode
+  case instr[0]
+  when 'acc' then next
+  when 'nop' then corruption[addr][0] = 'jmp'
+  when 'jmp' then corruption[addr][0] = 'nop'
+  end
+
+  result = execute corruption
+  if result[0] == TERMINATE
+    puts "Part 2: #{ result[1] }"
+    exit
+  end
+end
