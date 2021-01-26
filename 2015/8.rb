b70ff140d64bd2add40550af74b2502990b7c5d0
Matt Peperell <matt@peperell.com>
2021-01-26 01:12:37 +0000

    [2015:8] Solved part 2

--- 2015/8.rb
+++ 2015/8.rb
@@ -1,17 +1,32 @@
 lines = ARGF.readlines.map &:strip
 
 def unescape s
-  s.gsub(/(\\x..)|(\\")|(\\\\)/) { |sub|
+  s.gsub(/(\\x..)|(\\")|(\\\\)/) do |sub|
     case sub[1]
     when '"' then '"'
     when '\\' then '\\'
     when 'x' then sub[2..-1].to_i(16).chr
     else sub
     end
-  }[1..-2]
+  end[1..-2]
 end
 
-code = lines.map(&:chars).map(&:length).sum
-data = lines.map { |line| unescape(line.dup).length }.sum
+def escape s
+  middle = s.gsub(/["\\]/) do |sub|
+    case sub
+    when '"' then '\"'
+    when '\\' then '\\\\'
+    # when 'x' then sub[2..-1].to_i(16).chr
+    else sub
+    end
+end
+  %Q("#{middle}")
+end
 
-puts "Part 1: #{code-data}"
+original = lines.map(&:chars).map(&:length).sum
+unescaped = lines.map { |line| unescape(line.dup).length }.sum
+
+puts "Part 1: #{original - unescaped}"
+
+escaped = lines.map { |line| escape(line.dup).length }.sum
+puts "Part 1: #{escaped - original}"
