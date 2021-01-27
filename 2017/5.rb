384efac3c3c3a2dcc0886d400fcb7b1cccf90968
Matt Peperell <matt@peperell.com>
2021-01-27 16:11:56 +0000

    [2017:5] Solved part 2

--- 2017/5.rb
+++ 2017/5.rb
@@ -1,4 +1,6 @@
-jumps = ARGF.readlines.map &:to_i
+original = ARGF.readlines.map &:to_i
+
+jumps = original.dup
 
 ptr = 0
 steps = 0
@@ -11,3 +13,18 @@ while jumps[ptr]
 end
 
 puts "Part 1: #{steps}"
+
+
+jumps = original.dup
+
+ptr = 0
+steps = 0
+
+while jumps[ptr]
+  next_ptr = ptr + jumps[ptr]
+  jumps[ptr] += (jumps[ptr] >= 3 ) ? -1 : 1
+  steps += 1
+  ptr = next_ptr
+end
+
+puts "Part 2: #{steps}"
