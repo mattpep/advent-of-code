e3d75855c89a793478b95ee2dd0ac4313582ba3d
Matt Peperell <matt@peperell.com>
2021-01-27 16:31:34 +0000

    [2017:6] Solved part 1

---
+++ 2017/6.rb
@@ -0,0 +1,24 @@
+START_ALLOCATION = [4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5]
+
+def reallocate distribution
+  index_of_largest = distribution.index distribution.max
+  recipient = (index_of_largest + 1 ) % distribution.size
+  to_distribute = distribution[index_of_largest]
+  distribution[index_of_largest] = 0
+  while to_distribute > 0
+    distribution[recipient] += 1
+    recipient += 1
+    recipient %= distribution.size
+    to_distribute -= 1
+  end
+  distribution
+end
+
+banks = START_ALLOCATION.dup
+seen = [ banks.dup ]
+while seen.count( seen.last) == 1
+  banks = reallocate banks.dup
+  seen << banks.dup
+end
+
+puts "Part 1: #{seen.count - 1}"
