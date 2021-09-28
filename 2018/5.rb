9296e2eec5f05d6169d56dd7b5fd4bbaf6588832
Matt Peperell <mattp@automationlogic.com>
2021-09-28 13:42:51 +0100

    [2018:5] Solved part 2

--- 2018/5.rb
+++ 2018/5.rb
@@ -1,16 +1,26 @@
-polymer = ARGF.readline.strip
+original = ARGF.readline.strip
 
-changed = true
-
-while changed
-  changed = false
-  (0...polymer.length-1).each do |x|
-    if polymer[x].downcase == polymer[x+1].downcase && (polymer[x].ord - polymer[x+1].ord).abs == 32
-      polymer.replace( polymer[0...x] + polymer[x+2..-1].to_s)
-      changed = true
-      break
+def find_poly_length polymer
+  changed = true
+  while changed
+    changed = false
+    (0...polymer.length-1).each do |x|
+      if polymer[x].downcase == polymer[x+1].downcase && (polymer[x].ord - polymer[x+1].ord).abs == 32
+        polymer.replace( polymer[0...x] + polymer[x+2..-1].to_s)
+        changed = true
+        break
+      end
     end
   end
+  polymer.length
 end
-puts "Final length of polymer: #{polymer.size}"
 
+polymer = original.dup
+puts "part 1: Final length of polymer: #{find_poly_length polymer}"
+
+part2 = original.downcase.chars.uniq.sort.map do |remove|
+  polymer = original.dup
+  polymer.tr!("#{remove}#{remove.upcase}", '')
+  find_poly_length(polymer)
+end.min
+puts "part 2: #{part2}"
