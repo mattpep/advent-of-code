93cdd3df927376902a95602f7faf1ed49e1c74ba
Matt Peperell <matt@peperell.com>
2021-09-29 20:52:32 +0100

    [2015:14] Solved part 2

--- 2015/14.rb
+++ 2015/14.rb
@@ -3,7 +3,7 @@ PATTERN = /(?<name>[A-Z][a-z]+) can fly (?<speed>\d+) km\/s for (?<duration>\d+)
 RACE_DURATION = 2503
 reindeer = ARGF.readlines.each_with_object({}) do |record, deer|
   record.match(PATTERN) do |m|
-    deer[ m[:name] ] = { distance: 0, speed: m[:speed].to_i, dur: m[:duration].to_i, rest: m[:rest].to_i }
+    deer[ m[:name] ] = { distance: 0, speed: m[:speed].to_i, dur: m[:duration].to_i, rest: m[:rest].to_i, points: 0 }
   end
 end
 
@@ -14,7 +14,14 @@ RACE_DURATION.times do |tick|
       reindeer[name][:distance] += data[:speed]
     end
   end
+
+  leader_distance = reindeer.map { |r| r[1][:distance] }.max
+  leaders = reindeer.select { |name,data| data[:distance] == leader_distance }.keys
+  leaders.each { |r| reindeer[r][:points] += 1 }
 end
 
 winner = reindeer.max_by { |r| r[1][:distance]}
 puts "Part 1: #{winner[1][:distance]}"
+
+winner = reindeer.max_by { |r| r[1][:points]}
+puts "Part 2: #{winner[1][:points]}"
