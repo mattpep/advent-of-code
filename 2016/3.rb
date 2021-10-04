9c1dab46a40f933c1179f1f88c53d4eff332c0c0
Matt Peperell <mattp@automationlogic.com>
2021-10-04 16:44:31 +0100

    [2016:3] Solved part 2

--- 2016/3.rb
+++ 2016/3.rb
@@ -1,7 +1,7 @@
-triangles = ARGF.readlines.map(&:strip)
+triangles = ARGF.readlines.map(&:strip).map { |row| row.split.map &:to_i }
 
-part1 = triangles.select do |triangle|
-  sides = triangle.split.sort.map(&:to_i)
+part1 = triangles.select do |sides|
+  # sides = triangle.split.sort.map(&:to_i)
   sides.all? do |s|
     a = s
     b = sides.sum - s
@@ -10,3 +10,14 @@ part1 = triangles.select do |triangle|
 end.count
 
 puts "Part 1: #{part1}"
+
+
+part2 = triangles.each_slice(3).map(&:transpose).flatten(1).select do |sides|
+  sides.all? do |s|
+    a = s
+    b = sides.sum - s
+    a < b
+  end
+end.count
+
+puts "Part 1: #{part2}"
