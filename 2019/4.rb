a6dd4702bca24085d501dc5b27b07428a29b4d60
Matt Peperell <matt@peperell.com>
2021-10-07 00:18:44 +0100

    [2019:4] Solved part 2

--- 2019/4.rb
+++ 2019/4.rb
@@ -1,11 +1,25 @@
 RANGE = 197487..673251
 
-def validate n
-  # return false if n < 100_000 || n > 999_999
-  return false unless RANGE.include? n
-  return false unless n.digits.each_cons(2).any? { |a,b| a == b }
+STRICT = 0
+TOLERANT = 1
+
+def run_length_encode(i)
+  i.digits.slice_when { |x,y| x!= y}.map{ |x| [x.count, x[0]]}
+end
+
+def validate n, mode=TOLERANT
+  rle = run_length_encode(n)
+  if mode == STRICT
+    return false unless rle.any? { |count,_| count == 2 }
+  else
+    return false unless rle.any? { |count,_| count >= 2 }
+  end
+  # return false unless RANGE.include? n
   return true if n.digits.each_cons(2).all? { |b,a| a <= b }
 end
 
 part1 = RANGE.first.upto(RANGE.last).count { |x| validate x }
 puts "Part 1: #{part1}"
+
+part2 = RANGE.first.upto(RANGE.last).count { |x| validate x, STRICT }
+puts "Part 2: #{part2}"
