b4bf26f3dd14cf70fc6ab19ced8eb132b48342e4
Matt Peperell <matt@peperell.com>
2021-10-10 13:42:48 +0100

    [2015:11] Solved part 1

---
+++ 2015/11.rb
@@ -0,0 +1,15 @@
+INITIAL = 'cqjxjnds'
+
+def valid? password
+  return false unless password.chars.each_cons(3).any? { |a, b, c | b.ord == a.ord + 1 && c.ord == b.ord + 1 }
+  return false if 'oil'.chars.any? { |forbidden| password.include? forbidden }
+  return false unless password.chars.each_cons(2).select { |a, b| a == b }.flatten.uniq.length >= 2
+  true
+end
+
+password = INITIAL
+until valid? password
+  password.next!
+end
+
+puts "Part 1: #{ password }"
