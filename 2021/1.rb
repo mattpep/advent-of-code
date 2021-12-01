97f9aeb921454cb747152608fc8bf6e9254a2ee5
Matt Peperell <matt@peperell.com>
2021-12-01 09:04:56 +0000

    [2021:1] Don't interate needlessly in part 2

--- 2021/1.rb
+++ 2021/1.rb
@@ -3,5 +3,5 @@ data = ARGF.readlines.map &:to_i
 part1 = data.each_cons(2).map { |a, b| a < b }.count(true)
 puts "Part 1: #{part1}"
 
-part2 = data.each_cons(3).map(&:sum).each_cons(2).map { |a, b| a < b }.count(true)
+part2 = data.each_cons(4).map { |a, _, _, b| a < b }.count(true)
 puts "Part 2: #{part2}"
