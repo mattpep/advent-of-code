f7958802686d613a854d312764ecbbb466e14ac5
Matt Peperell <matt@peperell.com>
2021-12-07 13:36:20 +0000

    [2021:6] Solved part 2

--- 2021/6.rb
+++ 2021/6.rb
@@ -1,15 +1,23 @@
-fish = ARGF.readline.split(',').map &:to_i
+fish = ARGF.readline.split(',').map(&:to_i).each_with_object({}) do |f, hash|
+  hash[f] = hash[f].to_i.succ
+end
 
 NORMAL_DELAY = 6
 FIRST_DELAY = 8
-80.times do |i|
-  puts "population after #{i} days: #{fish.count}"
-  growth = fish.count(0)
-  fish.map! do |timer|
-    timer == 0 ? NORMAL_DELAY : timer.pred
+256.times do |i|
+  growth = fish[0].to_i
+  new_fish = fish.each_with_object({}) do |timer, hash|
+    if timer[0] == 0
+      hash[NORMAL_DELAY] = fish[0]
+    else
+      hash[timer[0].pred] = hash[timer[0].pred].to_i + fish[timer[0]]
+    end
   end
-  growth.times { fish.append FIRST_DELAY }
+  new_fish[FIRST_DELAY] = growth + new_fish[FIRST_DELAY].to_i
+  fish = new_fish
+  if i == 80.pred
+    puts "Part 1: #{fish.values.sum}"
+  end
 end
 
-# puts "after 80 days: fish are: #{fish}"
-puts fish.count
+puts "Part 2: #{fish.values.sum}"
