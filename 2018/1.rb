59528bbd5cf85beffdad6a859f1b149a43f3fb52
Matt Peperell <matt@peperell.com>
2021-12-11 01:04:30 +0000

    [2018:1] Solved part 2

--- 2018/1.rb
+++ 2018/1.rb
@@ -1,3 +1,16 @@
 numbers = ARGF.readlines.map(&:to_i)
 
 puts "Part 1: #{numbers.sum}"
+
+sums = []
+while true
+  numbers.each do |freq_change|
+    # puts "Looking at freq change: #{freq_change}"
+    sums << sums.last.to_i + freq_change
+    # puts "Just added #{sums.last}, freq sums are #{sums}"
+    if sums.count(sums.last) == 2
+      puts "Part 2: #{sums.last}"
+      exit
+    end
+  end
+end
