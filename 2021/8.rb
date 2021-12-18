e65495bdfd08da57283e5fee346ffe282989ab24
Matt Peperell <matt@peperell.com>
2021-12-18 22:34:03 +0000

    [2021:8] Solved part 2

--- 2021/8.rb
+++ 2021/8.rb
@@ -1,8 +1,27 @@
+VALID_PATTERNS = %w[
+  abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg
+]
+
+INPUT = 0
+OUTPUT = 1
+
 data = ARGF.readlines.map(&:strip).map do |line|
   input, output = line.split(' | ')
   [input.split, output.split]
 end
 
-part1 = data.map(&:last).flatten.select { |segments| [2,3,4,7].include? segments.length }.count
+part1 = data.map(&:last).flatten.count { |segments| [2,3,4,7].include? segments.length }
 
 puts "Part 1: #{part1}"
+
+def decode row
+  'abcdefg'.chars.permutation(7).each do |perm|
+    if row[INPUT].all? { |input| VALID_PATTERNS.include?(input.tr('abcdefg', perm.join).chars.sort.join) }
+      return row[OUTPUT].map {|digit| VALID_PATTERNS.index((digit.tr 'abcdefg', perm.join).chars.sort.join) }.join.to_i
+    end
+  end
+end
+
+
+part2 = data.sum { |record| decode record }
+puts "Part 2: #{part2}"
