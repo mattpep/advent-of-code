9b42f700efb45279ba04d5f9f0c94a261ed45613
Matt Peperell <matt@peperell.com>
2022-12-05 21:43:56 +0000

    [2022:5] Refactor ruby solution

--- 2022/5.rb
+++ 2022/5.rb
@@ -1,8 +1,18 @@
+# frozen_string_literal: true
+
 input = ARGF.read.split("\n\n")
 
-stacks = input[0].split("\n")[0...-1].map {|row| row.chars.each_slice(4).map{|c| c[1]}}.transpose.map(&:reverse).map{|stack| stack.reject{|c| c == ' '}}
-moves = input[1].split("\n").map {|row| row.split}.map {|fields| [fields[1], fields[3], fields[5]].map(&:to_i)}
+stacks = input[0].split("\n")[0...-1].map do |row| # skip the last row; it's the column numbering
+  row.chars.each_slice(4) # each column is 4 chars wide, of the form '[X] '
+     .map { |c| c[1] } # the character we care about is the second one, array index 1
+end.transpose.map(&:reverse).map { |stack| stack.reject { |c| c == ' ' } } # convert from row to column order
 
+# Move data is of the form
+#   move 3 from 1 to 3
+# Split into words, take the even fields and convert to numbers
+moves = input[1].split("\n").map do |row|
+  row.split.each_slice(2).map(&:last).map(&:to_i)
+end
 stacks1 = Marshal.load Marshal.dump(stacks)
 stacks2 = Marshal.load Marshal.dump(stacks)
 
