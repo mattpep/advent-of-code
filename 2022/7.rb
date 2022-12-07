c36ad157c80411a1e4088f3f8febbd864c76a43e
Matt Peperell <matt@peperell.com>
2022-12-07 18:45:39 +0000

    [2022:7] Solved part 1 in Ruby

---
+++ 2022/7.rb
@@ -0,0 +1,57 @@
+data = ARGF.readlines.map &:strip
+
+
+class Tree
+  attr_accessor :files, :directories, :name, :type, :parent
+
+  def initialize(name)
+    self.files = []
+    self.directories = []
+    self.name = name
+    self.parent = nil
+
+    if name == '/'
+      self.parent = self
+      # self.directories << self
+    end
+  end
+
+  def size
+    files.sum {|f| f[:size]} + directories.sum {|d| d.size}
+  end
+
+  def deep_dirs
+    directories + directories.map {|d| d.deep_dirs }.flatten
+  end
+end
+
+root = Tree.new("/")
+current = root
+data.each do |line|
+  if line.start_with? '$ cd'
+    target =  line[5..-1]
+    if target == ".."
+      current = current.parent
+      next
+    elsif target == "/"
+      current = root
+    else
+      current = current.directories.select {|d| d.name == target }.first
+    end
+  elsif line.start_with? '$ ls'
+    next
+  elsif line.start_with? 'dir '
+    dirname = line[4..-1]
+    d = Tree.new(dirname)
+    d.parent = current
+    current.directories << d
+  else
+    fields = line.split
+    size = fields[0].to_i
+    name = fields[1]
+    current.files << { name: name, size: size }
+  end
+end
+
+part1 = root.deep_dirs.select {|d| d.size <= 100000 }.map{|d| d.size }.sum
+puts "Part 1 (sum of all dirs <= 100000): #{part1}"
