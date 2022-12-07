68e3bd9732145f2ade0a1fadbc4ad3b340172c36
Matt Peperell <matt@peperell.com>
2022-12-07 19:07:08 +0000

    [2022:7] Solved part 2 in Ruby

--- 2022/7.rb
+++ 2022/7.rb
@@ -1,5 +1,7 @@
 data = ARGF.readlines.map &:strip
 
+MAXSIZE = 70_000_000
+NEEDED = 30_000_000
 
 class Tree
   attr_accessor :files, :directories, :name, :type, :parent
@@ -55,3 +57,9 @@ end
 
 part1 = root.deep_dirs.select {|d| d.size <= 100000 }.map{|d| d.size }.sum
 puts "Part 1 (sum of all dirs <= 100000): #{part1}"
+
+space_used = root.size
+space_free = MAXSIZE - space_used
+space_to_be_freed = NEEDED - space_free
+part2 = root.deep_dirs.select {|d| d.size >= space_to_be_freed }.map{|d| d.size }.min
+puts "Part 2 (size of smallest directory to be removed): #{part2}"
