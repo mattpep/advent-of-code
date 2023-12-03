93262c5ca5ec6503037e59a7ea07a70e5107b888
Matt Peperell <matt@peperell.com>
2023-12-03 00:22:29 +0000

    [2023:2] Solved both parts in Ruby

---
+++ 2023/2.rb
@@ -0,0 +1,47 @@
+games = ARGF.readlines.map(&:strip).each_with_object({}) do |row, hash|
+  parts = row.split(': ')
+  id = parts[0].split[1].to_i
+  hands = parts[1].split('; ').map do |hand|
+    colors = hand.split(', ').each_with_object({}) do |color,hash|
+      count_colour = color.split
+      hash[count_colour[1]] = count_colour[0].to_i
+    end
+  end
+  hash[id] = hands
+end
+
+CONSTRAINT = {
+  red: 12,
+  green: 13,
+  blue: 14
+}
+
+def hand_possible?(hand, constraint)
+  return hand.all? { |color, hand_count| hand_count <= constraint[color.to_sym] }
+end
+
+
+def game_possible?(game, constraint)
+  game.all? { |hand| hand_possible?(hand, constraint) }
+end
+
+def game_power(game)
+  CONSTRAINT.keys.map do |color|
+    game.map { |h| h[color.to_s].to_i }.max
+  end.reduce(:*)
+end
+
+part1 = games.sum do |id, game|
+  if game_possible?(game,CONSTRAINT)
+    id
+  else
+    0
+  end
+end
+
+puts "Part 1: #{part1}"
+
+part2 = games.sum do |_, game|
+  game_power(game)
+end
+puts "Part 2: #{part2}"
