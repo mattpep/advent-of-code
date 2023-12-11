7781398f170615c8003b0c0af112dc9de2269815
Matt Peperell <matt@peperell.com>
2023-12-11 00:40:37 +0000

    [2023:8] Solved part 2 in PHP

--- 2023/8.php
+++ 2023/8.php
@@ -1,4 +1,67 @@
 <?php
+class Rider
+{
+    public $distance;
+    private $paths;
+    private $directions;
+    private $location;
+
+    public function __construct($paths, $directions, $location) {
+        $this->paths = $paths;
+        $this->directions = $directions;
+        $this->location = $location;
+        $this->distance = 0;
+    }
+
+    public function step() {
+        $direction = $this->directions[$this->distance % strlen($this->directions)];
+        if ($direction == 'L')
+            $this->location = $this->paths[$this->location][0];
+        else
+            $this->location = $this->paths[$this->location][1];
+        $this->distance++;
+    }
+
+    public function go_home() {
+        while ( substr($this->location, -1) != 'Z' )
+            $this->step();
+    }
+}
+
+function get_start_nodes($path_info) {
+    $locations = array();
+    foreach ($path_info as $loc=>$dests)
+        if (substr($loc, -1) == 'A')
+            array_push($locations, $loc);
+    return $locations;
+}
+
+function gcd($a, $b) {
+    while ($b != 0) {
+        $r = $b;
+        $b = $a % $b;
+        $a = $r;
+    }
+    return $a;
+}
+
+function lcm($a, $b) {
+    if ($a == 0 || $b == 0) return 0;
+    $result = ($a * $b) / gcd($a, $b);
+    return abs($result);
+}
+
+function lcm_reduce($array) {
+    if (count($array) == 1)
+        return $array[0];
+    if (count($array) == 2)
+        return lcm($array[0], $array[1]);
+
+    $a = array_pop($array);
+    $b = array_pop($array);
+    array_push($array, lcm($a, $b));
+    return lcm_reduce($array);
+}
 
 $file = file('data/8.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
@@ -13,22 +76,18 @@ foreach(array_slice($file, 1) as $pathline) {
 }
 
 $START = 'AAA';
-$TARGET = 'ZZZ';
 
-$distance = 0;
-$location = $START;
-while ($location != $TARGET)
-{
-    $offset = $distance % strlen($directions);
-    $next_step = $directions[$distance % strlen($directions)];
-    if ($next_step == 'L')
-    {
-        $location = $paths[$location][0];
-    }
-    else {
-        $location = $paths[$location][1];
-    }
-    $distance++;
+$rider = new Rider($paths, $directions, $START);
+$rider->go_home();
+
+print("Part 1: $rider->distance\n");
+
+$distances = array();
+$riders = array();
+foreach (get_start_nodes($paths) as $loc) {
+    $rider = new Rider($paths, $directions, $loc);
+    $rider->go_home();
+    array_push($distances, $rider->distance);
 }
 
-print("Part 1: $distance\n");
+printf("Part 2: %d\n", lcm_reduce($distances));
